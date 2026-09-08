b23614bd80b0041a64ddaeaa1bf71af350d20d90
GawaliOm <gawali.om006@gmail.com>
2026-09-08 18:04:15 +0530

    Added logic to send form data to sample database for testing working

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
+from extentions import db
+from models.reportLost import LostItem
+from models.reportFound import FoundItem
+
 
 app = Flask(__name__)
 
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///lostincampus.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 # Function to load homepage
 
@@ -19,10 +28,28 @@ def reportLost():
         name = request.form["name"]
         item_name = request.form["item_name"]
         email = request.form["email"]
-        print("name: ", name)
-        print("item name: ", item_name)
-        print("email: ", email)
+        class_name = request.form["class_name"]
+        contact_number = request.form["contact_number"]
+        description = request.form["description"]
+        lost_location = request.form["lost_location"]
+        lost_date = request.form["lost_date"]
+
+        lost_item = LostItem(
+            name=name,
+            class_name=class_name,
+            contact_number=contact_number,
+            email=email,
+            item_name=item_name,
+            description=description,
+            lost_location=lost_location,
+            lost_date=lost_date
+        )
+
+        db.session.add(lost_item)
+        db.session.commit()
+
         return "lost report recived"
+    
     return render_template("reportLost.html")
 
 
@@ -35,13 +62,37 @@ def reportfound():
         name = request.form["name"]
         item_name = request.form["item_name"]
         email = request.form["email"]
-        print("name: ", name)
-        print("item name: ", item_name)
-        print("email: ", email)
+        contact_number = request.form["contact_number"]
+        description = request.form["description"]
+        found_location = request.form["found_location"]
+        found_date = request.form["found_date"]
+        collection_place = request.form["collection_place"]
+
+
+        found_item = FoundItem(
+            name = name,
+            item_name = item_name,
+            email = email,
+            contact_number = contact_number,
+            description = description,
+            found_location = found_location,
+            found_date = found_date,
+            collection_place = collection_place
+        )
+
+        db.session.add(found_item)
+        db.session.commit()
+
+
+        
         return "Found Report Sent"
     return render_template("reportFound.html")
 
 
 
 if __name__ == "__main__":
+
+    with app.app_context():
+        db.create_all()
+
     app.run(debug = True)
